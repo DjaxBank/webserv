45d53d8e419a835265615daceb6959a35dcbd3a6
Djax Bank <djax@djaxbank.com>
2026-01-20 16:06:53 +0100

    Refactor configuration handling: update config format and enhance error checking for missing directives

--- inc/config.hpp
+++ inc/config.hpp
@@ -1,14 +1,22 @@
 #pragma once
 #include <vector>
 #include <string>
+#include <exception>
 #include "UrlRule.hpp"
+
+struct socket_pair
+{
+	std::string		interface;
+	int				port;
+};
 
 class Config
 {
 	private:
 		Config();
-		void ImportIntPortPairs(std::ifstream fstream, size_t linecount);
-		std::vector<std::string[2]> socket_pairs;
+		void						ImportIntPortPairs(std::string value);
+		void	CheckAllFull();
+		std::vector<socket_pair>	socket_pairs;
 		std::string					Forbidden;
 		std::string					NotFound;
 		std::string					MaxRequestBodySize;
@@ -19,4 +27,14 @@ class Config
 		Config(const Config &other);
 		Config &operator=(const Config &other);
 		~Config();
+		class UnknownOptionException : public std::exception
+		{
+			public:
+				const char *what() const throw(){return "unknown directive at line ";};
+		};
+		class MissingOptionException : public std::exception
+		{
+			public:
+				const char *what() const throw(){return nullptr;};
+		};
 };
--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,20 @@ int main(int argc, char **argv)
 		std::cerr << "./webserv [configuration file]\n";
 		return EXIT_FAILURE;
 	}
-	Config Config(argv[1]);
-	// while (server_running)
-	// {
-		Socket sock(8080);
-		// accept(sock.get_socket_fd());
-	// }
+	try
+	{
+		Config Config(argv[1]);
+		bool server_running = true;
+		while (server_running)
+		{
+			server_running = false;
+			Socket sock(8080);
+			// accept(sock.get_socket_fd());
+		}
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+		return EXIT_FAILURE;
+	}
 }
