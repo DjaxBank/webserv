8b7257fbc070c07e5f8b2f41ef9b109a7beeff06
Djax Bank <djax@djax.dev>
2026-01-21 17:29:19 +0100

    accepts connections on ports. basic sockets working

--- src/handle_client.cpp
+++ src/handle_client.cpp
@@ -4,6 +4,7 @@
 #include <sys/select.h>
 #include <requestParser.hpp>
 #include <iostream>
+#include <unistd.h>
 
 static void receive_data(int clientfd)
 {
@@ -12,31 +13,33 @@ static void receive_data(int clientfd)
 	size_t bytes_read = 1;
 	std::cout << "new request from :" << "???" << "\n\n";
 	RequestParser parser;
-	size_t i = 0;
 	while(bytes_read > 0)
 	{
 		bytes_read = recv(clientfd, buf, 1024, 0);
 		std::string better_buf(buf, bytes_read);
-		if (better_buf.find("\r\n"))
-			bytes_read = 0;
-		std::cout << i++ << better_buf << '\n';
+		std::cout << better_buf << '\n';
 		parser.fetch_data(better_buf);
+		if (better_buf.find("\r\n"))
+			break ;
 	}
 }
 
 void handle_client(fd_set *socket_fds, std::vector<Socket> &sockets)
 {
-	std::vector<Socket> active_sockets;
 	std::vector<int>	client_fds;
-
+	
 	for (size_t i = 0 ; i < sockets.size() ; i ++)
-		if (FD_ISSET(sockets[i].get_socket_fd(), socket_fds))
-			active_sockets.push_back(sockets[i]);
-	for (size_t i = 0 ; i < active_sockets.size() ; i ++)
 	{
-		socklen_t addr_len = sizeof(struct sockaddr_in);
-		client_fds.push_back(accept(active_sockets[i].get_socket_fd(), reinterpret_cast <sockaddr *>(&active_sockets[i].get_addr()), &addr_len));
+		if (FD_ISSET(sockets[i].get_socket_fd(), socket_fds))
+		{
+			socklen_t addr_len = sizeof(struct sockaddr_in);
+			client_fds.push_back(accept(sockets[i].get_socket_fd(), reinterpret_cast <sockaddr *>(&sockets[i].get_addr()), &addr_len));
+		}
 	}
 	for (int &fd : client_fds)
+	{
 		receive_data(fd);
+	}
+	for (int &fd : client_fds)
+		close(fd);
 }
