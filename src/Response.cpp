7da3bad6ea8c331d515afa39512a4407e8a04516
Djax Bank <djax@djaxbank.com>
2026-02-02 18:03:00 +0100

    feat: implement content type detection in Response class and add HTML template

--- src/Response.cpp
+++ src/Response.cpp
@@ -15,9 +15,26 @@ std::string Response::get_timestr()
 	return buff;
 }
 
+void	Response::find_contentype()
+{
+	if (file_location.find_last_of('.') != file_location.npos)
+	{
+		const std::map<std::string, std::string> types = {
+			{".html", "text/html"},
+			{".css", "text/css"},
+			{".js", "application/javascript"},
+			{".png", "image/png"},
+			{".jpg", "image/jpeg"},
+			{".txt", "text/plain"}
+		};
+		auto it = types.find(file_location.substr(file_location.find_last_of('.')));
+		if (it != types.end())
+			content_type = it->second;
+	}
+}
+
 Response::Response(const Route_rule &route, const RequestParser &parser) : Date(get_timestr()), file_location(route.root + parser.getTarget())
 {
-
 	if (access(file_location.c_str(), F_OK) == -1)
 		status = "404 Not Found";
 	else if (access(file_location.c_str(), R_OK) == -1)
@@ -28,10 +45,24 @@ Response::Response(const Route_rule &route, const RequestParser &parser) : Date(
 
 void Response::Send(const int fd)
 {
-	std::string line;
+	std::string Response_header;
 
-	line = "HTTP/1.1 " + status + "\r\n";
-	write(fd, line.c_str(), line.length());
+	Response_header = "HTTP/1.1 " + status + "\r\n";
+	write(fd, Response_header.c_str(), Response_header.length());
+	std::cout << Response_header;
+	Response_header = "Date: " + Date + "\r\n";
+	write(fd, Response_header.c_str(), Response_header.length());
+	std::cout << Response_header;
+	find_contentype();
+	if (!content_type.empty())
+	{
+		Response_header = "Content-Type: " + content_type + "\r\n";
+		write(fd, Response_header.c_str(), Response_header.length());
+		std::cout << Response_header;
+	}
+	Response_header = "\r\n";
+	write(fd, Response_header.c_str(), Response_header.length());
+	std::cout << Response_header;
 	if (status == "200 OK")
 	{
 		std::ifstream file(file_location);
