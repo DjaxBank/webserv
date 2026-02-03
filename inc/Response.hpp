83d179bf12d1b733b1a5faabeb41d22cc7f8b29a
Djax Bank <djax@djaxbank.com>
2026-02-03 16:14:27 +0100

    update Response class

--- inc/Response.hpp
+++ inc/Response.hpp
@@ -1,22 +1,29 @@
 #include "Request.hpp"
 #include "route.hpp"
 #include "requestParser.hpp"
+#include "config.hpp"
 
 class Response
 {
 	private:
 		const int		fd;
 		std::string		status;
+		std::string		Forbiddenpage;
+		std::string		NotFoundPage;
 		HttpMethod		method;
 		std::string		content_type;
 		std::string		content_length;
 		std::string		Date;
 		std::string		file_location;
+		std::string		body;
 		Response();
+		std::string 	ExtractFile(std::string file_path, size_t *total_bytes);
+		void 			GET();
 		std::string		get_timestr();
 		bool			find_contentype();
 		void			Send(std::string data);
 	public:
-		Response(const Route_rule &route, const RequestParser &parser, const int fd, HttpMethod method);
+		Response(const Config &config, const Route_rule &route, const RequestParser &parser, const int fd, HttpMethod method);
 		void	Reply();
+		~Response();
 };
