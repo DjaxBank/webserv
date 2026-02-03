acd3585b9f8564ddc17e2e14438107ec77a7b4dd
Djax Bank <djax@djax.dev>
2026-02-03 10:52:11 +0100

    add connection close

--- src/Response.cpp
+++ src/Response.cpp
@@ -54,8 +54,9 @@ void Response::Send(std::string data)
 
 void Response::Reply()
 {
-	Send("HTTP/1.0 " + status + "\r\n");
+	Send("HTTP/1.1 " + status + "\r\n");
 	Send("Date: " + Date + "\r\n");
+	Send("Connection: close\r\n");
 	std::string body;
 	if (status == "200 OK")
 	{
