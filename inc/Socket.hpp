0b266449d56447748ffb9676fa29ae0865d4e9fe
Djax Bank <djax@djax.dev>
2026-02-12 14:26:40 +0100

    load correct site based on port

--- inc/Socket.hpp
+++ inc/Socket.hpp
@@ -12,10 +12,12 @@ class Socket
 		int					socket_fd;
 		struct sockaddr_in	addr;
 	public:
-		Socket() : socket_fd(-1){};
 		std::pair<int, std::string> info;
+		int client_fd;
+		Socket() : socket_fd(-1){};
 		Socket(std::pair<int, std::string> sock);
 		Socket(const Socket &other);
+		Socket &operator=(const Socket &other);
 		~Socket();
 		int get_socket_fd();
 		sockaddr_in &get_addr();
